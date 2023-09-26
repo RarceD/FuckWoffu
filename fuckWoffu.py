ecb6b5a0cc6f70642a204be3193c182ba11df2b8
RarceD <akimbo170@gmail.com>
2023-09-26 18:03:13 +0200

    Execute every 60s

--- fuckWoffu.py
+++ fuckWoffu.py
@@ -1,14 +1,18 @@
 import requests
 import json
+import time
 
-def get_bare_token():
+
+def get_json_data():
     with open('secrets.json', 'r') as file:
         json_content = json.load(file)
-        return json_content['bearerToken']
+        return json_content['bearerToken'], json_content['times']
 
+
+bearer_token, sign_times = get_json_data()
 url = 'https://perkinelmer.woffu.com/api/svc/signs/signs'
 headers = {
-    'Authorization': 'Bearer ' + get_bare_token(),
+    'Authorization': 'Bearer ' + bearer_token,
     'Content-Type': 'application/json'
 }
 payload = {
@@ -20,7 +24,8 @@ payload = {
     "timezoneOffset": -120
 }
 
-def fuck_woffu():
+
+def fuck_woffu() -> bool:
     response = requests.post(url, json=payload, headers=headers)
     if response.status_code == 200:
         print('Response:', response.json())
@@ -28,5 +33,26 @@ def fuck_woffu():
     else:
         return False
 
-print('fuck woffu script')
-fuck_woffu()
+
+def is_sign_time(signTimes: []) -> bool:
+    current_time = time.strftime("%H:%M")
+    if current_time in signTimes:
+        return True
+    return False
+
+
+'''
+    Every 60 seconds I check my enter/leave time and execute request
+'''
+if __name__ == "__main__":
+    while True:
+
+        if is_sign_time(sign_times):
+            success = fuck_woffu()
+            if success:
+                print('Fuck woffu -> ', time.strftime("%H:%M"))
+            else:
+                print('Error maybe something should be done ¯\_(ツ)_/¯ ',
+                      time.strftime("%H:%M"))
+
+        time.sleep(60)
