c0ecea71d3fbdf5cf2d26e2fd20899ca75b43021
RarceD <akimbo170@gmail.com>
2023-09-26 17:40:53 +0200

    Add bearer token

--- fuckWoffu.py
+++ fuckWoffu.py
@@ -4,7 +4,7 @@ import json
 def get_bare_token():
     with open('secrets.json', 'r') as file:
         json_content = json.load(file)
-        return json_content['bareToken']
+        return json_content['bearerToken']
 
 url = 'https://perkinelmer.woffu.com/api/svc/signs/signs'
 headers = {
@@ -23,7 +23,6 @@ payload = {
 def fuck_woffu():
     response = requests.post(url, json=payload, headers=headers)
     if response.status_code == 200:
-        print('POST request successful!')
         print('Response:', response.json())
         return True
     else:
